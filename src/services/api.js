dd941609d5c4e1b9873fef8ffab63e71fa17f9f6
dhenycruz <dheniarley.ds@gmail.com>
2022-11-10 18:30:11 -0300

    chore: confgirando endpoint de login e trails em services

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001/'
+    baseURL: 'https://squad12-orange-evolution-api.herokuapp.com'
 });
 
-export const requestLogin = async (endPoint,body)=>{
-    const {data} = await api.post(endPoint,body);
+export const requestLogin = async (body)=>{
+    const {data} = await api.post('/login', body);
     return data;
 }
 
-export const requestTrails = async (endPoint) => {
-    const { data } = await api.get(endPoint);
+export const requestTrails = async () => {
+    const { data } = await api.get('/trails');
     return data;
 };
