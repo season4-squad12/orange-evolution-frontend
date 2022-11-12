4838039a5a6e6f24ea12b9064375a89a7df1508d
dhenycruz <dheniarley.ds@gmail.com>
2022-11-11 22:43:22 -0300

    feat: finalizada  tela home

--- src/Components/Home/CardTrail.jsx
+++ src/Components/Home/CardTrail.jsx
@@ -4,7 +4,7 @@ import PolygonDown from '../../images/polygon-down.png';
 import { Image } from "../Image";
 import { Card, Divider, Divdrop, Div, } from './CartTrailStyles';
 
-export const CardTrail = (props) => {
+export const CardTrail = ({ trail }) => {
   const [drop, setDrop] = useState(false);
   
   const getDrop = () => {
@@ -12,16 +12,16 @@ export const CardTrail = (props) => {
   };
 
   return (
-      <Card borderColor={ props.color } drop={drop}>
-        <h1>Trilha UX/UI</h1>
-        <p>UX Design, UX Research, Design Thinking, Benchmarking, Persona, Mapa de Empatia, Fluxo de Usuário, Jornada do Usuário...</p>
+      <Card borderColor={trail.color} drop={drop}>
+        <h1>{trail.name}</h1>
+        <p>{ trail.description }</p>
         <Divider />
         <Div onClick={ getDrop }>
-          <h3>O que faz a pessoa UX/UI?</h3>
+          <h3>{trail.question}</h3>
           <Image src={ drop ? PolygonDown : Polygon } alt="imagem poligono" width="13px" height="13px"/>
         </Div>
         <Divdrop drop={drop}>
-          <p>Responsável pela experiência e navegação dos usuários em multiplataformas como websites e aplicações mobile.</p>
+          <p>{trail.response}</p>
         </Divdrop>
       </Card>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import { Header } from '../Components/Header';
 import { Button, ButtonAction, Footer, Main, Banner, CardList } from '../Components/Home/style';
 import { CardTrail } from '../Components/Home/CardTrail';
+import { requestTrialsHome } from '../services/api';
+import { useEffect } from 'react';
 
-const numeros = [
-  { id: 1, color: '#f225c0' },
-  { id: 2, color: '#00C19C' }, 
-  {id: 3, color: '#8819D2' },
-];
 
 export const Home = () => {
+  const [trails, setTrails] = useState([]);
   document.title = "Orange Evolution"
   const navigate = useNavigate();
+
+  const request = async () => {
+    const response = await requestTrialsHome();
+    setTrails(response);
+  }
+
+  const redirect = (url) => {
+    navigate(url);
+  };
+
+  useEffect(() => {
+    request();
+  }, []);
+
   return (
     <>
       <Main>
@@ -25,20 +37,21 @@ export const Home = () => {
             oferecemos uma gamificação do processo de ensino, exercícios e uma
             comunidade própria para interagir com outros alunos e tirar dúvidas!
           </p>
-          <ButtonAction onCLick={()=> navigate('/login')}>Quero Começar</ButtonAction>
+          <ButtonAction onClick={ () => redirect('/registrar')}>Quero Começar</ButtonAction>
         </Banner>
         <h3>CONHEÇA NOSSAS TRILHAS</h3>
         <CardList>
           {
-            numeros.map((item) => (
-              <CardTrail color={item.color} />
+            trails.length > 0 &&
+            trails.map((trail, index) => (
+              <CardTrail key={index} trail={trail} />
             ))
           }
         </CardList>
       </Main>
       <Footer>
-        <Button onCLick={() => navigate('/registrar')} color="white" background="#00C19C">REGISTRAR</Button>
-        <Button onClick={() => navigate('/login')} color="#001024" background="">LOGAR</Button>
+        <Button onClick={ () => redirect('/registrar')} color="white" background="#00C19C">REGISTRAR</Button>
+        <Button onClick={ () => redirect('/login')} color="#001024" background="">LOGAR</Button>
       </Footer>
     </>
   );
